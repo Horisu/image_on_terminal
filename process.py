51555f6fffb7f1dccf7f11f06a861cd4a6472b97
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2016-08-27 16:50:07 +0900

    refinement , made

--- process.py
+++ process.py
@@ -3,55 +3,62 @@ import cv2
 import os
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option("-i","--image", dest="image",
-                  help="original image", metavar="IMAGE")
-parser.add_option("-f","--file", dest="file",
-                  help="write to FILE", metavar="FILE")
-parser.add_option("--width", dest="width",
-                  help="output width", metavar="WIDTH")
-parser.add_option("--height", dest="height",
-                  help="output height", metavar="HEIGHT")
+
+class ImTerminal:
+
+    def __init__(self, width=None, height=None):
+        self.w = width
+        self.h = height
+
+    def convert(self, image):
+        h, w = image.shape[:2]
+
+        print "height = {}".format(h)
+        print "width  = {}".format(w)
+
+        b = 0
+        g = 1
+        r = 2
+
+        tmp = ""
+        for i in range(h):
+            for j in range(w):
+                tmp = tmp + '\\e[48;5;' + str( x256.from_rgb(image[i,j,r],image[i,j,g],image[i,j,b]) ) + "m" + "  "
+
+            tmp = tmp + '\\e[0m'
+            tmp = tmp + "\n"
+
+        return tmp
 
 
-(opt, args) = parser.parse_args()
 
-if not opt.image:
-    print "please specify image file"
-    exit()
-if not opt.file:
-    print "please specify output file"
-    exit()
+if __name__ == '__main__':
 
-if os.path.exists(opt.file):
-    print "{} already exists".format(opt.file)
-    os.remove(opt.file)
-    print "{} is removed".format(opt.file)
+    parser = OptionParser()
+    parser.add_option("-i","--image", dest="image", help="original image", metavar="IMAGE")
+    parser.add_option("-o","--output", dest="output", help="write to OUTPUT", metavar="OUTPUT")
+    parser.add_option("--width", dest="width", help="output width", metavar="WIDTH")
+    parser.add_option("--height", dest="height", help="output height", metavar="HEIGHT")
 
-image = cv2.imread(opt.image);
+    (opt, args) = parser.parse_args()
 
-if image is None:
-    print "not found such image file"
-    exit()
+    if not opt.image:
+        print "please specify image file"
+        exit()
+    if not opt.output:
+        print "please specify output file"
+        exit()
 
-h, w = image.shape[:2]
+    if os.path.exists(opt.output):
+        print "{} already exists".format(opt.output)
+        os.remove(opt.output)
+        print "{} is removed".format(opt.output)
+                    
+    image = cv2.imread(opt.image);
 
-print "height = {}".format(h)
-print "width  = {}".format(w)
+    f = open(opt.output, "w")
 
-b = 0
-g = 1
-r = 2
-
-f = open(opt.file, "w")
-
-for i in range(h):
-    tmp = ""
-    for j in range(w):
-        tmp = tmp + '\\e[48;5;' + str( x256.from_rgb(image[i,j,r],image[i,j,g],image[i,j,b]) ) + "m  "
-
-    tmp = tmp + '\\e[0m'
-    tmp = tmp + "\n"
-    f.write(tmp)
-
-f.close()
+    imt = ImTerminal(opt.width, opt.height)
+    abc = imt.convert(image)
+    f.write(abc)
+    f.close()
